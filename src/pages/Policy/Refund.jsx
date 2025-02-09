2bba3aaf419714dfab57a1feb8ab14f1064d5ead
Harsh <patelharsh749005@gmail.com>
2025-02-09 11:13:09 +0530

    navbar removed

--- src/pages/Policy/Refund.jsx
+++ src/pages/Policy/Refund.jsx
@@ -5,7 +5,7 @@ import Footer from "../../components/Footer";
 const Refund = () => {
   return (
     <>
-    <Navbar />
+ 
     <hr className="w-[90%] m-auto mt-2 text-gray-400" />
 
     <div className="h-100vh  w-full flex flex-col gap-5 mb-10">
